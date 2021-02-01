a0306f940775ceaf036cb9594bd6f106fc415b7b
Per Thykjaer Jensen <per@multimusen.dk>
2021-02-01 10:41:50 +0100

    dom exercise

---
+++ dom-experiment/js/js.js
@@ -0,0 +1,72 @@
+/**
+ * file: js.js
+ * what: class Y day 02 spring 2021
+ **/
+console.log('JavaScript from js/js.js: up and running!')
+
+/* DOM info */
+// console.log( window  )
+
+console.log( document )
+
+// the DOM will change the "virtual" html that the user sees
+// document.getElementById('content').innerHTML = "Mojn Verden"
+
+// we will add an image to the DOM
+let myImg = document.createElement("img")
+myImg.src = "images/helloween.jpg"
+let element = document.getElementById("div1")
+element.appendChild(myImg)
+
+/*
+<img src="images/anImage.png">
+*/
+
+/* Variables */
+
+let myString = "The world is wonderfull";
+let myString_2 = 'Cogito ergo sum';
+
+let myNumber = 777
+let number2 = 23
+
+let santaExists = true;
+let pixiesExist = false;
+
+let doIcare = null;
+
+console.log( myNumber )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
